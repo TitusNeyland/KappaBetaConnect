cc0c96920a29e646667c001d85ae5fbda5fc86ec
TitusNeyland <neylandtitus@yahoo.com>
2025-04-06 22:32:46 -0500

    Initial Commit

---
+++ KappaBetaConnect/KappaBetaConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  KappaBetaConnectApp.swift
+//  KappaBetaConnect
+//
+//  Created by Titus Neyland on 4/6/25.
+//
+
+import SwiftUI
+
+@main
+struct KappaBetaConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
