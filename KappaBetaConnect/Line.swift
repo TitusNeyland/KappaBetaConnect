18dfcb8bf8206832bfc62b2f3068a09dc6eff6b8
TitusNeyland <neylandtitus@yahoo.com>
2025-04-19 17:56:14 -0500

    [titusan] adding both a lines object and repository so that it can seamlessly integrate into the app

---
+++ KappaBetaConnect/Line.swift
@@ -0,0 +1,32 @@
+import Foundation
+import FirebaseFirestore
+
+struct Line: Codable, Identifiable {
+    var id: String?
+    var line_name: String
+    var semester: String
+    var year: Int
+    var members: [LineMember]
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case line_name
+        case semester
+        case year
+        case members
+    }
+}
+
+struct LineMember: Codable, Identifiable {
+    var id: String?
+    var name: String
+    var alias: String
+    var number: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case alias
+        case number
+    }
+} 
